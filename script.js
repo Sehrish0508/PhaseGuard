0c05558e5181597c4d9b995f9890c7c5da571b55
Sehrish0508 <sehrish.ntt@gmail.com>
2026-08-29 15:34:16 +0400

    Updated fetch URL to deployed Render Backend

--- script.js
+++ script.js
@@ -184,7 +184,7 @@ updateSineWave(200, 200, 200);
 // writing the fetch function with  async/await mode
 
 async function getPredictions(IaVal, IbVal, IcVal, VaVal, VbVal, VcVal) {
-    const response = await fetch("http://127.0.0.1:8000/predict", {
+    const response = await fetch("https://phaseguard-api.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type" : "application/json"},
         body: JSON.stringify({
